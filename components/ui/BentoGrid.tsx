dec5b5f4e0207c7b2d5347bcb7a109a9bb235596
dfsfyt <dfsfyt@163.com>
2024-10-07 19:03:27 +0800

    <img> -> <Image

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -7,6 +7,7 @@ import animationData from "@/data/confetti.json"
 import MagicButton from "./MagicButton";
 import { IoCopyOutline } from "react-icons/io5";
 import Lottie from "react-lottie";
+import Image from "next/image";
 
 // import dynamic from 'next/dynamic';
 
@@ -73,7 +74,7 @@ export const BentoGridItem = ({
       <div className={`${id === 6 && 'flex justify-center'} h-full`}>
         <div className='w-full h-full absolute'>
             {img && (
-                <img
+                <Image
                     src={img} 
                     alt={img}
                     className={cn(imgClassName, 'object-center')}
@@ -82,7 +83,7 @@ export const BentoGridItem = ({
         </div>
         <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
             {spareImg && (
-                <img
+                <Image
                     src={spareImg}
                     alt={spareImg}
                     className='object-cover object-center w-full h-full'
